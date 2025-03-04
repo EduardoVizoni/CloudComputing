90cc7e94c319c1143606f5c6f3c81304f670d5a6
Eduardo Vizoni do Prado <145796079+EduardoVizoni@users.noreply.github.com>
2025-03-03 21:53:13 -0300

    Ultimo commit

--- AWS/Config/S3Client.js
+++ AWS/Config/S3Client.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: 'us-east-1',
-  accessKeyId: 'AKIA5RRHCKYZTUINUDUY',
-  secretAccessKey: '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',
+  accessKeyId: '',
+  secretAccessKey: '',
 });
 
 const s3 = new AWS.S3();
